3a54a5d0620583d5406e92ada807119a79405db6
Panachai <panachai.ny@gmail.com>
2023-03-18 23:25:56 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -9,6 +9,7 @@ load_dotenv()
 
 url = os.getenv('URL')
 alt = os.getenv('ALT')
+folder = os.getenv('FOLDER')
 
 if not url:
     print('URL environment variable is not set')
@@ -16,11 +17,14 @@ if not url:
 elif not alt:
     print('ALT environment variable is not set')
     exit(1)
+elif not folder:
+    print('FOLDER environment variable is not set')
+    exit(1)
 
 
 listLink = scrapping.get_image_link_from(url, alt)
 
-dir.create_folder('images')
+dir.create_folder('images/' + folder)
 
 full_path = image.get_full_path('images/', 'file_name')
 image.download_image_v2(listLink[0], full_path)
